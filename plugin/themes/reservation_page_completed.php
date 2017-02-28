6eb0616ee3e264abe09dc3615b64c71bb85c7148
Yentl Wils <yentl.wils@gmail.com>
2017-03-01 00:14:03 +0100

    add completed string

--- plugin/themes/reservation_page_completed.php
+++ plugin/themes/reservation_page_completed.php
@@ -3,10 +3,11 @@
 	<?php global $iwb_settings; ?>
 	<div class="reservation-completed-content">
 		<h3 class="title"><?php echo __('Reservation Completed!', 'inwavethemes'); ?></h3>
-		<p><?php echo isset($iwb_settings['general']['reservation_completed_message']) ? stripslashes($iwb_settings['general']['reservation_completed_message']) : ''; ?></p>
+		<p><?php echo sprintf(__('Your reservation details have just been sent to %s. If you have any question, please do not hesitate to contact us. Thank you!', 'inwavethemes'), $booking->getMember()->email); ?></p>
 		<div class="contact">
 			<span><?php echo isset($iwb_settings['general']['reservation_completed_contact_phone']) ? stripslashes($iwb_settings['general']['reservation_completed_contact_phone']) : ''; ?></span>
 			<span><?php echo isset($iwb_settings['general']['reservation_completed_contact_email']) ? stripslashes($iwb_settings['general']['reservation_completed_contact_email']) : ''; ?></span>
 		</div>
 	</div>
+</div>
 </div>
